6043f9fef6ceaedafe3112d01b98c440d3d40588
郑茜 <zhenq021@gmail.com>
2020-06-21 14:28:59 +0800

    TagsSection.tsx JS部分基本完成

--- src/views/Copper/TagsSection.tsx
+++ src/views/Copper/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
   flex-grow: 1;
   display: flex;
   justify-content: flex-end;
@@ -20,6 +21,9 @@ const TagsSection = styled.section`
     padding: 1px 14px;
     font-size: 16px;
     margin: 3px 6px;
+    &.selected {
+    background: #edfa00;
+    }
 }
 }
 > button {
@@ -32,4 +36,33 @@ const TagsSection = styled.section`
   }
 `;
 
-export {TagsSection}
+const TagsSection: React.FC = () => {
+  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const onAddTag = () => {
+    const tagName = window.prompt('请输入标签名');
+    if (tagName !== null) {
+      setTags([...tags, tagName]);
+    }
+  };
+  const onToggleTag = (tag: string) => {
+    const index = selectedTags.indexOf(tag);
+    if (index >= 0) {
+      setSelectedTags(selectedTags.filter(t => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+  const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map(tag =>
+          <li key={tag} onClick={() => onToggleTag(tag)} className={getClass(tag)}>{tag}</li>
+        )}
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>
+  );
+};
+export {TagsSection};
