126285b6eb8f40959e52b7e09d7fa3b5568eb43e
郑茜 <zhenq021@gmail.com>
2020-06-23 23:27:08 +0800

    抽离 useTags 函数

--- src/views/Copper/TagsSection.tsx
+++ src/views/Copper/TagsSection.tsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 import React, {useState} from 'react';
-
+import {useTags} from 'useTags';
 
 type Props = {
   value: string[]
   onChange: (selected: string[]) => void
 }
 const TagsSection: React.FC<Props> = (props) => {
-  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const {tags, setTags} = useTags();
   const selectedTags = props.value;
   const onAddTag = () => {
     const tagName = window.prompt('请输入标签名');
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
 import Layout from '../components/Layout';
+import React, {useState} from 'react';
+import {useTags} from 'useTags';
 
 function Tags() {
+  const {tags, setTags} = useTags();
   return (
     <Layout>
-      <h2>标签页</h2>
-    </Layout>);
+      <ol>
+        {tags.map(tag =>
+          <li key={tag}>{tag}</li>
+        )}
+      </ol>
+    </Layout>
+  );
 }
 
 export default Tags;
