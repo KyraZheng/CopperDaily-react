1af2705d7db7d6c242d0fbbae641acecbe7dcaff
郑茜 <zhenq021@gmail.com>
2020-06-24 16:10:46 +0800

    完成 TagList 样式

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,16 +1,56 @@
 import Layout from '../components/Layout';
-import React, {useState} from 'react';
+import React from 'react';
 import {useTags} from 'useTags';
+import styled from 'styled-components';
+
+const TagList = styled.ol`
+  font-size: 16px;
+
+  > li{
+    margin: 12px;
+    background: #f5f5f5;
+    border: 1px solid #777;
+    border-radius: 10px;
+    line-height: 20px;
+    padding: 8px 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    > span{
+       display: inline;
+    }
+  }
+`
+
+const Button = styled.button`
+  margin-top: 16px;
+  font-size: 16px;
+  border: none;
+  background: #bababa;
+  padding: 8px 12px;
+  border-radius: 12px;
+
+`
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 function Tags() {
   const {tags, setTags} = useTags();
   return (
     <Layout>
-      <ol>
+      <TagList>
         {tags.map(tag =>
-          <li key={tag}>{tag}</li>
+          <li key={tag}>{tag} <span> → </span> </li>
         )}
-      </ol>
+      </TagList>
+      <Center>
+        <Button>新增标签</Button>
+      </Center>
+
     </Layout>
   );
 }
