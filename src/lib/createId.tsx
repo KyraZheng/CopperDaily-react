1da097520d9248917f2b90561502c368c7f6bf85
郑茜 <zhenq021@gmail.com>
2020-06-26 23:25:37 +0800

    数据持久化

--- src/lib/createId.tsx
+++ src/lib/createId.tsx
@@ -1,7 +1,9 @@
-let id = 0;
+let id = parseInt(window.localStorage.getItem('idMax') || '0');
 const createId = () => {
   id += 1;
-  return id
-}
 
-export {createId}
+  window.localStorage.setItem('idMax', JSON.stringify(id));
+  return id;
+};
+
+export {createId};
--- src/views/Copper/TagsSection.tsx
+++ src/views/Copper/TagsSection.tsx
@@ -1,21 +1,15 @@
 import styled from 'styled-components';
 import React from 'react';
 import {useTags} from 'useTags';
-import {createId} from '../../lib/createId';
+
 
 type Props = {
   value: number[]
   onChange: (selected: number[]) => void
 }
 const TagsSection: React.FC<Props> = (props) => {
-  const {tags, setTags} = useTags();
+  const {tags, addTag} = useTags();
   const selectedTagIds = props.value;
-  const onAddTag = () => {
-    const tagName = window.prompt('请输入标签名');
-    if (tagName !== null) {
-      setTags([...tags, {id:createId(), name:tagName}]);
-    }
-  };
   const onToggleTag = (tagId: number) => {
     const index = selectedTagIds.indexOf(tagId);
     if (index >= 0) {
@@ -32,7 +26,7 @@ const TagsSection: React.FC<Props> = (props) => {
           <li key={tag.id} onClick={() => onToggleTag(tag.id)} className={getClass(tag.id)}>{tag.name}</li>
         )}
       </ol>
-      <button onClick={onAddTag}>新增标签</button>
+      <button onClick={() => {addTag()}}>新增标签</button>
     </Wrapper>
   );
 };
